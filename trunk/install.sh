5c6d4a97fe16755e3b1e17b3d86f0d7462ab1397
pchev <pchev@7773a146-defb-4110-8bb2-b7bfa8bda9ad>
2015-05-02 20:11:42 +0000

    Set a separate project for eqmodgui

--- trunk/install.sh
+++ trunk/install.sh
@@ -26,7 +26,6 @@ install -m 755 -d $destdir/share/icons/hicolor/scalable
 install -m 755 -d $destdir/share/icons/hicolor/scalable/apps
 
 install -v -m 755 -s src/ccdciel  $destdir/bin/ccdciel
-install -v -m 755 -s src/eqmodgui  $destdir/bin/eqmodgui
 install -v -m 644 system_integration/Linux/share/applications/ccdciel.desktop $destdir/share/applications/ccdciel.desktop
 install -v -m 644 system_integration/Linux/share/appdata/ccdciel.appdata.xml $destdir/share/appdata/ccdciel.appdata.xml
 install -v -m 644 system_integration/Linux/share/doc/ccdciel/changelog $destdir/share/doc/ccdciel/changelog
@@ -34,6 +33,3 @@ install -v -m 644 system_integration/Linux/share/doc/ccdciel/copyright $destdir/
 install -v -m 644 system_integration/Linux/share/pixmaps/ccdciel.png $destdir/share/pixmaps/ccdciel.png
 install -v -m 644 system_integration/Linux/share/icons/hicolor/48x48/apps/ccdciel.png $destdir/share/icons/hicolor/48x48/apps/ccdciel.png
 install -v -m 644 system_integration/Linux/share/icons/hicolor/scalable/apps/ccdciel.svg $destdir/share/icons/hicolor/scalable/apps/ccdciel.svg
-install -v -m 644 system_integration/Linux/share/applications/eqmodgui.desktop $destdir/share/applications/eqmodgui.desktop
-install -v -m 644 system_integration/Linux/share/pixmaps/eqmodgui.png $destdir/share/pixmaps/eqmodgui.png
-install -v -m 644 system_integration/Linux/share/icons/hicolor/48x48/apps/eqmodgui.png $destdir/share/icons/hicolor/48x48/apps/eqmodgui.png
