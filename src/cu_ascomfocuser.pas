bda7decbed382fd8ee148b20a971c753018f6d1c
Patrick Chevalley <pch@ap-i.net>
2016-05-27 07:09:40 +0000

    new constant location

--- src/cu_ascomfocuser.pas
+++ src/cu_ascomfocuser.pas
@@ -25,7 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 interface
 
-uses cu_focuser, u_global,
+uses cu_focuser, u_global, indiapi,
     {$ifdef mswindows}
     Variants, comobj,
     {$endif}
