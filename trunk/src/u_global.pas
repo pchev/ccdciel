a262b6115d6d8c6174c2af115bb973e12a1fb57d
pchev <pchev@7773a146-defb-4110-8bb2-b7bfa8bda9ad>
2017-04-29 08:51:06 +0000

    Set version 0.8.12

--- trunk/src/u_global.pas
+++ trunk/src/u_global.pas
@@ -160,7 +160,7 @@ type
   {$i revision.inc}
 
 const
-  ccdcielver = '0.8.11';
+  ccdcielver = '0.8.12';
   ccdciel_version='Version beta '+ccdcielver;
   blank=' ';
   clOrange=$1080EF;
--- trunk/system_integration/MacOSX/readme.txt
+++ trunk/system_integration/MacOSX/readme.txt
@@ -1,5 +1,5 @@
 CCDciel
-Version 0.8.11 beta
+Version 0.8.12 beta
 
 
 Copyright (C) 2017 Patrick Chevalley
