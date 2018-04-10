b039a8e524b038765b65b2e91191b0f99f1ce1d3
Patrick Chevalley <pch@ap-i.net>
2018-04-10 11:26:10 +0200

    Update documentation

--- doc/getdoc.sh
+++ doc/getdoc.sh
@@ -2,9 +2,9 @@
 
 rm -rf doc wiki_doc
 
-wget -E -p -np -nH --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc --restrict-file-names=windows -k http://www.ap-i.net/ccdciel/en/start http://www.ap-i.net/ccdciel/en/start --header="X-DokuWiki-Do: export_xhtml"
+wget -E -p -np -nH --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc --restrict-file-names=windows -k https://www.ap-i.net/ccdciel/en/start https://www.ap-i.net/ccdciel/en/start --header="X-DokuWiki-Do: export_xhtml"
 
-wget -E -r -p -np -nH --timeout=15 --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc --cache=off --restrict-file-names=windows -k  http://www.ap-i.net/ccdciel/en/documentation/start http://www.ap-i.net/ccdciel/en/documentation/start --header="X-DokuWiki-Do: export_xhtml"
+wget -E -r -p -np -nH --timeout=15 --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc --cache=off --restrict-file-names=windows -k  https://www.ap-i.net/ccdciel/en/documentation/start https://www.ap-i.net/ccdciel/en/documentation/start --header="X-DokuWiki-Do: export_xhtml"
 
 rm doc/robots.txt
 
