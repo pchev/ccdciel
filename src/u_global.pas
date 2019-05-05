2a9c91d64b534b753178dfbc852dea97fb829b77
Patrick Chevalley <pch@ap-i.net>
2019-05-05 13:46:30 +0200

    Set version 0.9.56

--- src/u_global.pas
+++ src/u_global.pas
@@ -186,7 +186,7 @@ type
   {$i revision.inc}
 
 const
-  ccdcielver = '0.9.55';
+  ccdcielver = '0.9.56';
   ccdciel_version='Version beta '+ccdcielver;
   TargetFileVersion = 4;
   Maxclient = 100;
