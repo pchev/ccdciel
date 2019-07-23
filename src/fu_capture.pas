8ce3a140c30b8a2f4ec3ef353aa694e31f4d393f
pchev <pch@ap-i.net>
2019-07-23 20:54:28 +0200

    Only for Light frames, otherwise Flat auto-exposure do not wrok

--- src/fu_capture.pas
+++ src/fu_capture.pas
@@ -171,7 +171,7 @@ begin
           Mount.SlewToDomeFlatPosition;
     end;
     if Assigned(FonMsg) then FonMsg(rsStartCapture,2);
-    EarlyNextExposure:=ConfigExpEarlyStart;
+    EarlyNextExposure:=(TFrameType(FrameType.ItemIndex)=LIGHT) and ConfigExpEarlyStart;
     if Assigned(FonStartExposure) then FonStartExposure(self);
     if (not Frunning) and Assigned(FonMsg) then FonMsg(rsCannotStartC,0);
   end else begin
