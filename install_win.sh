489527695673fe4f9dec5f9236d8fca8f7d93cd3
Patrick Chevalley <pch@ap-i.net>
2019-11-10 14:05:33 +0100

    No more install dcraw on Windows, libraw work fine.

--- install_win.sh
+++ install_win.sh
@@ -28,14 +28,12 @@ if [ $OS_TARGET = win32 ]; then
   strip -v -o $destdir/ccdciel.exe src/ccdciel.exe 
   strip -v -o $destdir/libccdcielwcs.dll library/wcs/libccdcielwcs.dll
   strip -v -o $destdir/libpasraw.dll library/raw/libpasraw.dll
-  strip -v -o $destdir/dcraw.exe library/dcraw/dcraw.exe
   unzip -d $destdir system_integration/Windows/data/openssl-win32.zip
 fi
 if [ $OS_TARGET = win64 ]; then
   strip -v -o $destdir/ccdciel.exe src/ccdciel.exe 
   strip -v -o $destdir/libccdcielwcs.dll library/wcs/libccdcielwcs.dll
   strip -v -o $destdir/libpasraw.dll library/raw/libpasraw.dll
-  strip -v -o $destdir/dcraw.exe library/dcraw/dcraw.exe
   unzip -d $destdir system_integration/Windows/data/openssl-win64.zip
 fi
 
