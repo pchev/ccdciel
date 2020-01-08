33cfe2ac00cd01c304f052ddf932e697965aa860
Patrick Chevalley <pch@ap-i.net>
2020-01-08 13:33:18 +0100

    Set version 0.9.68

--- src/u_global.pas
+++ src/u_global.pas
@@ -202,7 +202,7 @@ type
   {$i revision.inc}
 
 const
-  ccdcielver = '0.9.67';
+  ccdcielver = '0.9.68';
   ccdciel_version='Version beta '+ccdcielver;
   TargetFileVersion = 4;
   Maxclient = 100;
