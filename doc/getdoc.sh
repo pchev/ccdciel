f680342c14295aaeaabd127e26dd25e9c9f1f805
Patrick Chevalley <pch@ap-i.net>
2020-01-25 17:11:51 +0100

    Update documentation

--- doc/getdoc.sh
+++ doc/getdoc.sh
@@ -2,10 +2,10 @@
 
 rm -rf doc wiki_doc
 
-wget -E -p -np -nH --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc --restrict-file-names=windows -k https://www.ap-i.net/ccdciel/en/start https://www.ap-i.net/ccdciel/en/start --header="X-DokuWiki-Do: export_xhtml"
-wget -E -p -np -nH --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc --restrict-file-names=windows -k https://www.ap-i.net/ccdciel/en/features https://www.ap-i.net/ccdciel/en/features --header="X-DokuWiki-Do: export_xhtml"
+wget -E -p -np -nH --retry-on-http-error=503 --timeout=15 --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc --restrict-file-names=windows -k https://www.ap-i.net/ccdciel/en/start https://www.ap-i.net/ccdciel/en/start --header="X-DokuWiki-Do: export_xhtml"
+wget -E -p -np -nH --retry-on-http-error=503 --timeout=15 --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc --restrict-file-names=windows -k https://www.ap-i.net/ccdciel/en/features https://www.ap-i.net/ccdciel/en/features --header="X-DokuWiki-Do: export_xhtml"
 
-wget -E -r -p -np -nH --timeout=15 --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc --cache=off --restrict-file-names=windows -k  https://www.ap-i.net/ccdciel/en/documentation/start https://www.ap-i.net/ccdciel/en/documentation/start --header="X-DokuWiki-Do: export_xhtml"
+wget -E -r -p -np -nH --retry-on-http-error=503 --timeout=15 --cut-dirs=1 -X */*detail,*/*export,*/playground -R *do=index* -P doc --cache=off --restrict-file-names=windows -k  https://www.ap-i.net/ccdciel/en/documentation/start https://www.ap-i.net/ccdciel/en/documentation/start --header="X-DokuWiki-Do: export_xhtml"
 
 rm doc/robots.txt
 
