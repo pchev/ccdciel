cea184d38c5d3f4e91ee4ed671bfd016c22b5a3d
Patrick Chevalley <pch@ap-i.net>
2021-04-19 17:45:33 +0200

    Set version 0.9.75

--- src/u_global.pas
+++ src/u_global.pas
@@ -187,7 +187,7 @@ type
   {$i revision.inc}
 
 const
-  ccdcielver = '0.9.74';
+  ccdcielver = '0.9.75';
   ccdciel_version='Version beta '+ccdcielver;
   TargetFileVersion = 5;
   Maxclient = 100;
