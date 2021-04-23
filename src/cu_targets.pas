d802821680b1bb4c1450c4d9636b92e00d0e12f6
Patrick Chevalley <pch@ap-i.net>
2021-04-23 20:20:18 +0200

    Mount is not necessarily present

--- src/cu_targets.pas
+++ src/cu_targets.pas
@@ -2286,7 +2286,7 @@ function T_Targets.StartGuider:boolean;
 begin
  result:=false;
  if Autoguider=nil then exit;
- if not Mount.Tracking then exit;
+ if (mount.Status=devConnected)and(not mount.Tracking) then exit;
   msg(rsStartAutogui,2);
   Autoguider.Guide(true);
   result:=Autoguider.WaitGuiding(CalibrationDelay+SettleMaxTime);
