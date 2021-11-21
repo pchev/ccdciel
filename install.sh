2c4c8dcace386defbc8a769f4c63501b0b67100f
Mattia Verga <mattia.verga@tiscali.it>
2021-11-21 14:42:48 +0100

    New main icon

--- install.sh
+++ install.sh
@@ -24,8 +24,12 @@ install -m 755 -d $destdir/share/doc/ccdciel
 install -m 755 -d $destdir/share/pixmaps
 install -m 755 -d $destdir/share/icons
 install -m 755 -d $destdir/share/icons/hicolor
+install -m 755 -d $destdir/share/icons/hicolor/32x32
+install -m 755 -d $destdir/share/icons/hicolor/32x32/apps
 install -m 755 -d $destdir/share/icons/hicolor/48x48
 install -m 755 -d $destdir/share/icons/hicolor/48x48/apps
+install -m 755 -d $destdir/share/icons/hicolor/96x96
+install -m 755 -d $destdir/share/icons/hicolor/96x96/apps
 install -m 755 -d $destdir/share/icons/hicolor/scalable
 install -m 755 -d $destdir/share/icons/hicolor/scalable/apps
 install -m 755 -d $destdir/share/ccdciel
@@ -101,4 +105,7 @@ install -v -m 644 system_integration/Linux/share/metainfo/net.ap_i.ccdciel.metai
 install -v -m 644 system_integration/Linux/share/doc/ccdciel/changelog $destdir/share/doc/ccdciel/changelog
 install -v -m 644 system_integration/Linux/share/doc/ccdciel/copyright $destdir/share/doc/ccdciel/copyright
 install -v -m 644 system_integration/Linux/share/pixmaps/ccdciel.png $destdir/share/pixmaps/ccdciel.png
+install -v -m 644 system_integration/Linux/share/icons/hicolor/32x32/apps/ccdciel.png $destdir/share/icons/hicolor/32x32/apps/ccdciel.png
 install -v -m 644 system_integration/Linux/share/icons/hicolor/48x48/apps/ccdciel.png $destdir/share/icons/hicolor/48x48/apps/ccdciel.png
+install -v -m 644 system_integration/Linux/share/icons/hicolor/96x96/apps/ccdciel.png $destdir/share/icons/hicolor/96x96/apps/ccdciel.png
+install -v -m 644 system_integration/Linux/share/icons/hicolor/scalable/apps/ccdciel.svg $destdir/share/icons/hicolor/scalable/apps/ccdciel.svg
