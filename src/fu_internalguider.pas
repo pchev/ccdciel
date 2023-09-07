56d6d7e67ab89d1db0a5d5fbc00e4964e18a5ef7
Patrick Chevalley <pch@ap-i.net>
2023-09-07 11:58:59 +0200

    Remove extra spacing

--- src/fu_internalguider.pas
+++ src/fu_internalguider.pas
@@ -1085,7 +1085,7 @@ procedure Tf_internalguider.ButtonCalibrateClick(Sender: TObject);
 var txt: string;
     n: integer;
 begin
- txt:= rsSelectACalib+#10+#10+rsOption1Calib+#10+#10{+rsOption2Calib}+#10+#10+rsOption3Backl+#10+#10+rsOption4Cance;
+ txt:= rsSelectACalib+#10+#10+rsOption1Calib+#10+#10+rsOption3Backl+#10+#10+rsOption4Cance;
  {$ifdef lclgtk2}
  // inverted button with GTK2
     n:=QuestionDlg (rsGuiderCalibr, txt, mtCustom,
