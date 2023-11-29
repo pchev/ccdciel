8b026fbaf8447e47caac33f7895b8b7060921b76
Patrick Chevalley <pch@ap-i.net>
2023-11-29 18:57:59 +0100

    Add example how to use only a part of the parameters

--- scripts/astrometry-blind.sh
+++ scripts/astrometry-blind.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
 # Example script to run astrometry.net blind, ignoring all the other parameters set by CCDciel
-# parse $param if you need some specific parameters
-#
+# Parse $param to get the parameters that can be added selectively to the command
+# There is two example of command, uncomment the one you need
+
 
 if [ $# -ne 2 ]; then
   echo wrong number of parameters
   exit 1
 fi
 
+# arguments from CCDciel, the first with the filename, the second with all the other options for solve-field
 infile=$1
 param=$2
 
-echo "Use blind solving"
-echo "solve-field --overwrite --no-plots $infile"
+# split solve-field argument and declare a variable for each one
+set -- $param
+while [ $# -gt 0 ]; do
+   if [[ $1 == *"--"* ]]; then  # option start with --
+        v="${1/--/}"            # remove -- from name
+        v="${v/-/}"             # variable name cannot contain -
+        declare $v="$2"         # set the new variable
+   fi
+  shift
+done
 
-solve-field --overwrite --no-plots $infile
+## real blind solving without any options
+#echo "Use blind solving"
+#echo "solve-field --overwrite --no-plots $infile"
+#solve-field --overwrite --no-plots $infile
+
+# half blind solving using scale but no coordinates
+echo "Use half blind solving"
+echo "solve-field --overwrite --no-plots --scale-low $scalelow --scale-high $scalehigh --scale-units $scaleunits $infile"
+solve-field --overwrite --no-plots --scale-low $scalelow --scale-high $scalehigh --scale-units $scaleunits $infile
 
 exit $?
