6cc5fc13b03c6e1f612de2512918a9583bfd5e6f
Patrick Chevalley <pch@ap-i.net>
2025-01-19 13:47:16 +0100

    Add script for spectra processing with specINTI

---
+++ scripts/specinti/specinti_process.cmd
@@ -0,0 +1,38 @@
+@echo off
+
+rem This batch file is started by specinti_process.script to process the spectra in background and save the result
+  
+set specintiprogdir=%1
+set specintiprog=%2
+set specinticonfig=%3
+set obj=%4
+set workdir=%5
+set capturedir=%6
+set resultdir=%7
+set archivedir=%8
+set logfile=%9
+
+if not defined specintiprogdir exit /b 1
+if not defined specintiprog exit /b 1
+if not defined specinticonfig exit /b 1
+if not defined obj exit /b 1
+if not defined workdir exit /b 1
+if not defined capturedir exit /b 1
+if not defined resultdir exit /b 1
+if not defined archivedir exit /b 1
+if not defined logfile exit /b 1
+
+echo %date% %time:~0,8% : Start processing for %obj% >> %logfile%
+
+cd %specintiprogdir%
+%specintiprog% %specinticonfig%
+
+if errorlevel 1 ( 
+  echo %date% %time:~0,8% : Error %obj% >> %logfile%
+) else (
+  move %workdir%\_%obj%* %resultdir% 
+  move %capturedir%\%obj%* %archivedir%
+  echo %date% %time:~0,8% : Finish processing of %obj% >> %logfile%
+)
+
+timeout 30
---
+++ scripts/specinti/specinti_process.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This batch file is started by specinti_process.script to process the spectra in background and save the result
+
+specintiprogdir=${1}
+specintiprog=${2}
+specinticonfig=${3}
+obj=${4}
+workdir=${5}
+capturedir=${6}
+resultdir=${7}
+archivedir=${8}
+logfile=${9}
+wine_prefix=${10}
+
+if [[ -z $logfile ]]; then 
+  echo Missing parameters
+  exit 1; 
+fi
+
+echo "$(date) : Start processing for $obj" >> $logfile
+
+if [[ -n $wine_prefix ]]; then export WINEPREFIX=$wine_prefix; fi
+
+cd $specintiprogdir
+wine $specintiprog $specinticonfig
+
+if [[ $? = 0 ]]; then 
+  lobj=$(echo $obj | tr [A-Z] [a-z] )
+  mv $workdir/_$lobj* $resultdir
+  mv $capturedir/$obj* $archivedir
+  echo "$(date) : Finish processing of $obj" >> $logfile
+else
+  echo "$(date) : Error $obj" >> $logfile
+fi
+
+echo Close in 30 sec ...
+sleep 30
