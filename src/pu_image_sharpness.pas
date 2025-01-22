b2118969e822c51a3cbd4e175ed58529832df75e
Patrick Chevalley <pch@ap-i.net>
2025-01-22 09:37:44 +0100

    Improvement to image sharpness calculation, by Han

--- src/pu_image_sharpness.pas
+++ src/pu_image_sharpness.pas
@@ -4,7 +4,7 @@ unit pu_image_sharpness;
 {The routine applies the Root mean Square on the differences between the minimum and maximum value of each 2x2x4(RGGB) pixel combination of the image.}
 {The result is reversed and scaled such that the final result is roughly identical to the star HFD measurement.}
 
-{Copyright (C) 2020 by Han Kleijn www.hnsky.org and Patrick Chevalley http://www.ap-i.net, pch@ap-i.net }
+{Copyright (C) 2025 by Han Kleijn www.hnsky.org and Patrick Chevalley http://www.ap-i.net, pch@ap-i.net }
 
 {This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -23,65 +23,42 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, math;
 
 type
   image_array = array of array of array of single;
 
-function image_sharpness(img: image_array): double;{measure the sharpeness of an image. Result is reversed and scaled to be roughly identical to a HFD measurment. So value decreases with sharpness}
+function image_sharpness(img: image_array): double;// Measure the sharpeness of an image by gradient. The result is reversed and scaled to be roughly identical to a HFD curve So value decreases with sharpness
 
 implementation
 
 
-function image_sharpness(img: image_array): double;{measure the sharpeness of an image. Result is reversed and scaled to be roughly identical to a HFD measurment. So value decreases with sharpness}
-var
-  w,h, i,j:integer;
-  maxA,maxB,minA,minB,v1,v2,minimum,maximum,average : double;
+function image_sharpness(img: image_array): Double;// Measure the sharpeness of an image by gradient. The result is reversed and scaled to be roughly identical to a HFD curve So value decreases with sharpness
+Var
+  x, y, Width, Height         : Integer;
+  Sharpness, Center, Up, Right: double;
+
 begin
-//  nrcolor:=length(img);{nr colours}
-  h:=length(img[0,0]);{length}
-  w:=length(img[0]);{width}
+  Height:=length(img[0]);
+  Width:=length(img[0,0]);
 
-  result:=0;
-  average:=0;
+  Sharpness := 0;
+  for y := 2 to Height - 3 do // Loop over the image, excluding edges where step size of 2 cannot be applied
+  begin
+    for x := 2 to Width - 3 do
+    begin
+      Center := Img[0, y, x];// Center pixel value
+      Up :=Img[0, y + 2, x];// Neighboring pixel values with step size of 2. So compare red sensitive with red sensitive, green sensitive with green sensitive.
+      Right := Img[0, y, x + 2];
+      Sharpness:=Sharpness+sqr(up-center)+sqr(right-center);
+    end;//for loop
+  end;//for loop
 
- {for OSC and mono images}
-  for i:=0 to (w-4) div 4 do  {step in width}
-  for j:=0 to (h-4) div 4 do  {step 4 pixels in height}
-  begin {process 16 pixels. Test 2x2x(R+G+G+B) pixels}
-    v1:=(img[0,i*4   ,j*4]+
-         img[0,i*4+1 ,j*4]+
-         img[0,i*4   ,j*4+1]+
-         img[0,i*4+1 ,j*4+1]);{Sum of R+G+G+B}
-    v2:=(img[0,i*4+2  ,j*4]+
-         img[0,i*4+1+2,j*4]+
-         img[0,i*4+2  ,j*4+1]+
-         img[0,i*4+1+2,j*4+1]);{Sum of R+G+G+B}
-
-    if v1>v2 then begin maxA:=v1; minA:=v2; end else begin maxA:=v2; minA:=v1; end;{find the minimum and maximum values of the two bottom (R+G+G+B) combinations}
-
-    v1:=(img[0,i*4   ,j*4+2]+
-         img[0,i*4+1 ,j*4+2]+
-         img[0,i*4   ,j*4+1+2]+
-         img[0,i*4+1 ,j*4+1+2]);{Sum of R+G+G+B}
-    v2:=(img[0,i*4+2  ,j*4+2]+
-         img[0,i*4+1+2,j*4+2]+
-         img[0,i*4+2  ,j*4+1+2]+
-         img[0,i*4+1+2,j*4+1+2]);{Sum of R+G+G+B}
-    if v1>v2 then begin maxB:=v1; minB:=v2; end else begin maxB:=v2; minB:=v1; end; ;{find the minimum and maximum values of the two top (R+G+G+B)combinations}
-
-    if minA<minB then minimum:=minA else minimum:=minB;
-    if maxA>maxB then maximum:=maxA else maximum:=maxB;
-
-    result:=result+sqr(maximum-minimum);{square the local slope in the 2x2x(R+G+G+B) test area and add to the total}
-    average:=average+(maximum+minimum);
-  end;
-
-  result:=sqrt(result/(w*h));{slope value, highest value is the sharpest image}
-  average:=average/(w*h);{calculate average pixel value}
-  result:=4*average/result;{turn the curve upside down and scale simular as HFD values. A lower value indicates a sharper image}
+  if Sharpness>0 then
+    result := 100*width*height/sqrt(Sharpness) //turn the curve upside down and scale simular as HFD values. A lower value indicates a sharper image
+  else
+    result:=9999;//dummy value for case sharpness is zero
 end;
-
 
 end.
 
