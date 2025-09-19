21ed6a7da835f62af96a8445931afd56efa952ec
Patrick Chevalley <pch@ap-i.net>
2025-09-19 22:26:47 +0200

    Sync with CdC version

--- component/downloaddialog/downloaddialog.pas
+++ component/downloaddialog/downloaddialog.pas
@@ -250,6 +250,10 @@ var
   pos: TPoint;
   i: integer;
 begin
+  if SSLImplementation=TSSLNone then begin
+    ShowMessage('Please install the libssl3 package from your distribution');
+    exit;
+  end;
   FResponse := '';
   Ffirsturl := Furl;
   DF := TForm.Create(Self);
@@ -525,7 +529,7 @@ end;
 procedure TDownloadDialog.HTTPComplete;
 var
   ok: boolean;
-  i: integer;
+  i,k: integer;
   newurl: string;
   abuf: string;
 begin
@@ -556,6 +560,11 @@ begin
       if UpperCase(copy(http.Headers[i], 1, 9)) = 'LOCATION:' then
       begin
         newurl := trim(copy(http.Headers[i], 10, 9999));
+        if uppercase(copy(newurl,1,4))<>'HTTP' then begin
+          // relative redirection
+          k:=pos(':',Furl);
+          newurl:=copy(Furl,1,k)+'//'+http.TargetHost+newurl;
+        end;
         if (newurl = Furl) or (newurl = Ffirsturl) then
           ok := False
         else
